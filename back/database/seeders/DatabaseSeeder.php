a02bbe8e8c65177809c8c540782c69b549e682ab
kivicms <ivanovv1972@gmail.com>
2025-02-15 18:19:22 +0500

    Update network and container names in docker-compose.yml
    Renamed network and container references from "aa_oren" to "memory_book" for consistency. Ensures alignment with project naming conventions and avoids potential conflicts.

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -33,13 +33,13 @@ class DatabaseSeeder extends Seeder
         Role::create(['name' => 'admin', 'title' => 'Администратор']);
         $admin = User::factory()->create([
             'name' => 'kivicms',
-            'email' => 'ivanovv1972@gmail.com',
+            'email' => 'admin@memory-book.space',
             'phone' => '79619379273',
             'password' => '$2y$12$OX.AHE6rres4dlVrwbBDmexPU58cYALeZr28uiuLGkJtGBKdEs9lW', // Zc4e72xs
-            'notification_email' => 'ivanovv1972@gmail.com',
-            'first_name' => 'Виталий',
+            'notification_email' => 'admin@memory-book.space',
+            'first_name' => 'Кирилл',
             'last_name' => 'Иванов',
-            'middle_name' => 'Владимирович',
+            'middle_name' => 'Витальевич',
 
         ]);
         Profile::query()->create(['user_id' => $admin->id]);
